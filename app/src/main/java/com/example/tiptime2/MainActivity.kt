74129df45fbbf620b0a12ac81b2e7b5e8927f072
허석 <gjtjr0707@naver.com>
2021-05-15 03:48:21 +0900

    수정사항 발생

--- app/src/main/java/com/example/tiptime2/MainActivity.kt
+++ app/src/main/java/com/example/tiptime2/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 대충 소스코드에 수정 사항을 주기 위한 주석문 작성
     }
 }
